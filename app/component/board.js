20ac5d47ccef8d9799d96d6b84028f36991d0b16
Eric Nelson <big89e@gmail.com>
2017-08-12 17:28:40 -0400

    [DARTS-8] Colorize targets

--- app/component/board.js
+++ app/component/board.js
@@ -35,13 +35,24 @@ export default class Board extends Component {
               callback = this.props.onHit;
               disabled = false;
           }
+
+          let colors = boardColors[target.getLabel()];
+          if(disabled) {
+              colors = inactive;
+          }
           rows.push(
-                  <View key={targeti} style={{flex: 1, flexDirection: 'column', backgroundColor: theColors[targeti]}}>
+                  <View key={targeti} style={{
+                      flex: 1,
+                      flexDirection: 'column',
+                      borderRadius: 4,
+                      borderWidth: 5,
+                      borderColor: colors.border,
+                      backgroundColor: colors.background}}>
                       <TargetProgress label={target.getLabel()}
                                     hits={target.hits()}
                                     cleared={target.getDoneRound()}
                                     callback={callback}
-                                    disabled={disabled} 
+                                    disabled={disabled}
                                     />
                   </View>
           );
@@ -51,14 +62,18 @@ export default class Board extends Component {
   }
 };
 
-theColors = [
-    'powderblue',
-    'skyblue',
-    'steelblue',
-    'blue',
-    'powderblue',
-    'skyblue',
-    'steelblue',
-    'blue'
-];
+inactive = {border: '#f5f5f5', background: '#f5f5f5'};
+beige = {border: '#e29836', background: '#f9e8d2'};
+black = {border: '#333333', background: '#e6e6e6'};
+green = {border: '#1f7a1f', background: '#d6f5d6'};
+boardColors = {
+    '20': black,
+    '19': beige,
+    '18': black,
+    '17': beige,
+    '16': beige,
+    '15': beige,
+    'B': green
+};
+
 AppRegistry.registerComponent('Board', () => Board);
