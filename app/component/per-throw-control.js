fb5cab9f3b3f49834719a39bae9e8f2497849e25
Eric Nelson <big89e@gmail.com>
2017-08-12 21:29:15 -0400

    [DARTS-8] Added buttons with children

--- app/component/per-throw-control.js
+++ app/component/per-throw-control.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Button from 'apsl-react-native-button'
 import {
-    Button,
-    View
+    View,
+    Text
 } from 'react-native';
 
 
@@ -13,14 +14,21 @@ export default class PerThrowControl extends Component {
   render() {
     return (
         <View style={{flex: 1, flexDirection: 'row'}}>
-            <Icon
-                name="facebook"
-                />
             <Button
                 onPress={this.props.onHit}
                 title={"Hit"}
+                textStyle={{fontSize: 18}}
                 accessibilityLabel={"Hit target"}
                 color="#841584"
+            ><View><Icon
+                name="check-circle-o"
+                style={{color: 'green'}}
+            /><Text>2Hit2</Text>
+            </View>
+            </Button>
+            <Icon
+                name="times-circle-o"
+                style={{color: 'red'}}
             />
             <Button
                 onPress={this.props.onMiss}
